24391e45b1dd92a0587e31e17894f9a21640cfb0
abiudbenitez <abiudmisael10@hotmail.com>
2024-04-16 22:51:36 -0600

    modificaciones menores

--- src/main/java/cap1/Main.java
+++ src/main/java/cap1/Main.java
@@ -2,6 +2,20 @@ package cap1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String ian = "si es niño es Miguel";
+        String alfredo = "si es niño es Miguel";
+        int Miguel = ian.length();
+        String s = ian.toUpperCase();
+        String i = alfredo.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(s);
+        sb.append(i);
+
+        System.out.println(ian.equals(alfredo));
+        System.out.println(ian.lastIndexOf('u'));
+        System.out.println(alfredo.indexOf("es"));
+        System.out.println(Miguel + s + i + sb);
+        System.out.println(ian.substring(4));
     }
 }
